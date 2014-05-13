3dd6584ac2387fd5103a07aa9b8640172e76b238
Vsevolod Livinskij <vsevolod.livinskij@frtk.ru>
2014-05-13 18:48:20 +0400

    New verion without broadcast

--- net_integral/client.py
+++ net_integral/client.py
@@ -1,8 +1,14 @@
 import socket
 import sys
 import select
+import hello
 
 bufferSize = 1024
+
+def foo(task):
+    res = hello.count(task [0], task [1], task [2])
+    return res
+
 
 class Client():
     def __init__ (self, host, port):
@@ -21,23 +27,28 @@ class Client():
             self.socket.close()
             sys.exit (1)
  
-    def send (self, msg):
-        self.socket.sendall (msg)
-        print self.socket.recv(bufferSize)
+    def send (self):
+        msg = self.socket.recv(bufferSize)
+        data = msg.split ()
+        data [0], data [1] = float (data [0]), float (data [1])
+        data [2] = int (data [2])
+        print msg
+        print data
+        self.socket.sendall (str (foo ((data [0], data [1], data [2]))))
  
 ########################################################################
  
-#HOST = "127.0.0.1"
+HOST = "127.0.0.1"
 PORT_UDP = 10345
 PORT_TCP = 11345
 
-s = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
-s.bind(("", PORT_UDP))
-#result = select.select([s],[],[])
-#msg = result[0][0].recv(bufferSize) 
-msg, addr = s.recvfrom (bufferSize)
-print "Server IP: " + msg
+#s = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+#s.bind(("", PORT_UDP))
+##result = select.select([s],[],[])
+##msg = result[0][0].recv(bufferSize) 
+#msg, addr = s.recvfrom (bufferSize)
+#print "Server IP: " + msg
 
-client = Client (msg, PORT_TCP)
-client.send("haha")
+client = Client (HOST, PORT_TCP)
+client.send()
 client.socket.close()
--- net_integral/server.py
+++ net_integral/server.py
@@ -1,17 +1,21 @@
+import argparse
 import sys 
 import socket 
 import threading
+import time
 
 bufferSize = 1024
 max_connections = 100
+client_list = []
+delay = 10
 
 class Server():
-    def __init__ (self, host, port):
+    def __init__ (self, host, port, a, b, N):
         try:
             self.socket = socket.socket (socket.AF_INET, socket.SOCK_STREAM)
+            self.a, self.b, self.N = a, b, N
         except socket.error as e:
             print "Can't create server"
-            
         self.bind (host, port)
         
     def bind (self, host, port):
@@ -23,36 +27,70 @@ class Server():
             self.socket.close()
             sys.exit (1)
             
+    def get_client (self):
+        time_start = time.time ()
+        while (time.time () - time_start < delay):
+            try:
+                self.socket.setblocking(0)
+                client_socket, client_addr = self.socket.accept()
+                GetClient (client_socket)
+            except socket.error as e:
+                pass
+    
     def serve (self):
-        client_socket, client_addr = self.socket.accept()
-        ClientHandler (client_socket)
+        dec = (self.b - self.a) / (len (client_list) * 1.0)
+        prev = self.a
+        task = []
+        for i in range (len (client_list)):
+            task.append (prev)
+            prev += dec 
+        task.append (prev)
+        for i in range (len (client_list)):
+            ServeClient (client_list [i], task [i], task [i+1], self.N / len (client_list))
         
-
-class ClientHandler (threading.Thread):
+class GetClient (threading.Thread):
     def __init__ (self, client_socket):
         self.socket = client_socket
-        super(ClientHandler, self).__init__()
+        super(GetClient, self).__init__()
         self.start()
         
     def run (self):
-        msg = self.socket.recv (bufferSize)
-        print msg
+        client_list.append (self.socket)
+
+class ServeClient (threading.Thread):
+    def __init__ (self, client_socket, a, b, N):
+        self.socket = client_socket
+        self.a, self.b, self.N = a, b, N
+        super(ServeClient, self).__init__()
+        self.start()
+        
+    def run (self):
+        msg = str (self.a) + " " + str (self.b) + " " + str (self.N)
         self.socket.sendall (msg)
-        msg = self.socket.recv (bufferSize)
-        print msg
+        print self.socket.recv(bufferSize)
+        
         
 ########################################################################
-HOST = "192.168.1.45"
+HOST = "127.0.0.1"
 PORT_UDP = 10345
 PORT_TCP = 11345
 
-dest = ('<broadcast>', PORT_UDP)
-s = socket.socket (socket.AF_INET, socket.SOCK_DGRAM)
-s.bind (('', 0))
-s.setsockopt (socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
-s.setsockopt (socket.SOL_SOCKET, socket.SO_BROADCAST, 1)
-s.sendto (HOST, dest)
+parser = argparse.ArgumentParser (description = "Integral with Simpson's method spread to net")
+parser.add_argument("a", help = "start value", type = int)
+parser.add_argument("b", help = "end value", type = int)
+parser.add_argument("N", help = "step number", type = int)
+args = parser.parse_args()
 
-server = Server (HOST, PORT_TCP)
+#dest = ('<broadcast>', PORT_UDP)
+#s = socket.socket (socket.AF_INET, socket.SOCK_DGRAM)
+#s.bind (('', 12345))
+#s.setsockopt (socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
+#s.setsockopt (socket.SOL_SOCKET, socket.SO_BROADCAST, 1)
+#msg = HOST
+#s.sendto (msg, dest)
+
+server = Server (HOST, PORT_TCP, args.a, args.b, args.N)
+server.get_client ()
+print client_list
 server.serve ()
 server.socket.close ()
